335c9c63681db36b6625b3c6a713968f03451fc8
ThuanChau22 <thuan.q.chau@gmail.com>
2022-10-06 21:09:57 -0700

    Try hosting 1

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM composer as builder
+WORKDIR /
+COPY composer.* ./
+RUN composer install
+
+FROM php:8.1-apache
+RUN docker-php-ext-install mysqli && docker-php-ext-enable mysqli
+RUN apt-get update && apt-get upgrade -y
+WORKDIR /var/www/html
+COPY . ./
+COPY --from=builder /vendor /var/www/html/src/vendor
---
+++ src/app.php
@@ -0,0 +1,43 @@
+<?php
+require_once("config.php");
+require_once("utils.php");
+
+$conn = new mysqli($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASS"], $_ENV["DB_NAME"]);
+if ($conn->connect_error) {
+  echo $conn->connect_error;
+}
+
+$value = "tc";
+$stmt = $conn->prepare("SELECT * FROM users WHERE name = ?");
+$stmt->bind_param("s", $value);
+$stmt->execute();
+$result = $stmt->get_result();
+$stmt->close();
+if (!$result) {
+  echo $result;
+}
+
+for ($i = 0; $i < $result->num_rows; $i++) {
+  $result->data_seek($i);
+  $row = $result->fetch_assoc();
+  $name = $row["name"];
+  $age = $row["age"];
+}
+$port = $_SERVER["SERVER_PORT"];
+
+$result->close();
+$conn->close();
+
+echo <<<_HTML
+<html>
+  <head>
+    <title>Joe's Java</title>
+    <style></style>
+  </head>
+  <body>
+    <p>Name: $name</p>
+    <p>Age: $age</p>
+    <p>Running on port: $port</p>
+  </body>
+</html>
+_HTML;
