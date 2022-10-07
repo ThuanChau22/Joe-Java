2367df16850fb031522fb9821a0c5a363102efbf
ThuanChau22 <thuan.q.chau@gmail.com>
2022-10-07 03:02:29 -0700

    Added error page

---
+++ src/error.php
@@ -0,0 +1,15 @@
+<?php
+echo <<<_HTML
+<html>
+  <head>
+    <title>Joe's Java | Error</title>
+    <link rel="stylesheet" href="./styles/error.css">
+  </head>
+  <body>
+    <div class="center">
+      <h1>404</h1>
+      <h1>Not Found</h1>
+    </div>
+  </body>
+</html>
+_HTML;
