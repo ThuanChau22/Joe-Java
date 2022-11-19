edb43c52c15e10a6db8077d0ba0e0d422d370387
ThuanChau22 <thuan.q.chau@gmail.com>
2022-11-18 19:21:46 -0800

    Updated footer styles

--- src/components/footer.php
+++ src/components/footer.php
@@ -4,8 +4,9 @@ function footer()
   $currentYear = date("Y");
   return <<<FOOTER
   <footer id="footer">
-    <p class="copyright">© $currentYear All rights reserved.</p>
-    <p class="disclaimer">(CMPE-272 Course Project)</p>
+    <p class="footer-text">
+      © $currentYear. All rights reserved. CMPE-272 Course Project
+    </p>
   </footer>
   FOOTER;
 }
