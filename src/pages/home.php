a143386c80e078ed6b26644ffeff1cec220e2e1a
ThuanChau22 <thuan.q.chau@gmail.com>
2022-11-30 01:59:35 -0800

    Fixed reference link

--- src/pages/home.php
+++ src/pages/home.php
@@ -18,13 +18,13 @@ $content = <<<CONTENT
     <div class="col-md-4">
       <img class="home-guide-image img-responsive img-center" src="../../assets/home/coffee-bag.png" alt="">
       <p class="home-guide-content">1. Select beans that suits your taste.</p>
-      <a class="home-guide-link" href="/products?filters=coffee-beans">Explore</a>
+      <a class="home-guide-link" href="/products?category=coffee-beans">Explore</a>
       <div class="pb-5"></div>
     </div>
     <div class="col-md-4">
       <img class="home-guide-image img-responsive img-center" src="../../assets/home/coffee-machine.png" alt="">
       <p class="home-guide-content">2. Pick your brewing gear of choice.</p>
-      <a class="home-guide-link" href="/products?filters=brewing-tools">Explore</a>
+      <a class="home-guide-link" href="/products?category=brewing-tools">Explore</a>
       <div class="pb-5"></div>
     </div>
     <div class="col-md-4">
